d2af64274856d0874a00630fee5788a3d59489cb
Bilal raza <anwarbilal617@gmail.com>
2024-12-08 19:24:17 +0500

    added

--- uiux_hackathon/src/components/Banner3.tsx
+++ uiux_hackathon/src/components/Banner3.tsx
@@ -1,13 +1,16 @@
-import React from "react";
-
 const Banner3 = ({ name }: { name: string }) => {
   return (
-    <div>
-      <h1>{name}</h1>
-      <div>
-        <span>Home</span>
-        <span>.Pages</span>
-        <span>{name}</span>
+    <div className="bg-gray-200 h-80 flex flex-col items-center justify-center">
+      <div className="w-[60%]">
+        <h1
+          className="text-4xl font-extrabold text-[#101750]"
+          style={{ fontWeight: "800" }}
+        >
+          {name}
+        </h1>
+        <p className="text-black">
+          Home . Pages <span className="text-[#FB2E86]">. {name}</span>
+        </p>
       </div>
     </div>
   );
